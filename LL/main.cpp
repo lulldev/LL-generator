c04cb188fc7a830a7f185214bb519fdd71818205
lulldev <lulldev@yandex.ru>
2018-08-08 13:28:45 +0300

    init template

--- LL/main.cpp
+++ LL/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <boost/algorithm/string/regex.hpp>
+#include <boost/optional.hpp>
+#include <boost/regex.hpp>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    
+    if (argc != 2) {
+        cout << "Invalid arguments count\n"
+        << "Usage: program <input file>\n";
+        return 1;
+    }
+    
+    ifstream inputFile(argv[1]);
+    
+    if (!inputFile.is_open()) {
+        cout << "Error reading file " << argv[1] << endl;
+        return 1;
+    }
+    
     return 0;
 }
