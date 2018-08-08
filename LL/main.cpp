dea6dd5245a935e2963798be0fac81927a0bfd2f
lulldev <lulldev@yandex.ru>
2018-08-08 12:20:25 +0300

    Initial Commit

---
+++ LL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LL
+//
+//  Created by Ivan Kalagin on 08.08.2018.
+//  Copyright © 2018 Ivan Kalagin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
