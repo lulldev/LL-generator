d673a4e3bcc8c66ba33d7ad09c35b50d9aedd646
lulldev <lulldev@yandex.ru>
2018-08-08 13:40:28 +0300

    add scripts for running and reading input

---
+++ LL/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  run.sh
+#  LL
+#
+#  Created by Ivan Kalagin on 08.08.2018.
+#  Copyright © 2018 Ivan Kalagin. All rights reserved.
+
+cd /Users/lulldev/Library/Developer/Xcode/DerivedData/LL-fsptvsbkjykkfafewablnynqjljl/Build/Products/Debug/
+./LL /Users/lulldev/Documents/GitHub/LL-generator/LL/input.txt
